1a340ece42c5b25eb966942a6900071408d7bf4f
edave64 <edave64@googlemail.com>
2014-02-25 21:13:29 +0100

    Made function node-safe

--- js-runtime/function.js
+++ js-runtime/function.js
@@ -1,16 +1,30 @@
 (function () {
     "use strict";
 
-    Moo.JS.Function = function (func) {
+    var proto, constructor,
+        func, obj;
+
+    constructor = function (func) {
         /* Yes, you shouldn't use __proto__, implement a better way */
-        func.__proto__ = Moo.JS.Function.prototype;
+        func.__proto__ = constructor.prototype;
         return func;
     };
 
-    var proto = Moo.JS.Function.prototype = Object.create(Moo.JS.Object.prototype);
-    proto['|'] = Moo.JS.Function(function (self) {
-        return Moo.JS.Function(function (other) {
-            return Moo.JS.Function(function (param) {
+    /* node switch */
+    if (typeof module !== 'undefined') {
+        module.exports = constructor;
+        obj = require('./object');
+        func = require('./function');
+    } else {
+        Moo.JS.Function = constructor;
+        func = Moo.JS.Function;
+        obj = Moo.JS.Object;
+    }
+
+    proto = constructor.prototype = Object.create(obj.prototype);
+    proto['|'] = func(function (self) {
+        return func(function (other) {
+            return func(function (param) {
                 return self(other(param));
             })
         })
